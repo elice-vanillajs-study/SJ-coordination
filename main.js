cb416e6dfa8876a23af0d227cc3608678398bb1b
blingblin-g <luvbliny@gmail.com>
2021-08-19 13:40:00 +0900

    refactor css

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const vertical = document.querySelector('.vertical');
-const horizontal = document.querySelector('.horizontal');
-const target = document.querySelector('.target');
-const tag = document.querySelector('.tag');
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
 
-document.addEventListener('mousemove', (event) => {
-	const x = event.clientX;
-	const y = event.clientY;
-	console.log(`${event.clientX} ${event.clientY}`);
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(`${event.clientX} ${event.clientY}`);
 
-	vertical.style.left = `${x}px`;
-	horizontal.style.top = `${y}px`;
-	target.style.left = `${x}px`;
-	target.style.top = `${y}px`;
-	tag.style.left = `${x}px`;
-	tag.style.top = `${y}px`;
-	tag.innerHTML = `${x}px, ${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
-
